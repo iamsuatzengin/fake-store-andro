682d2ce0c35cacca75a44e2c8112181de6a4f1cf
suat <zenginsuat.inf@gmail.com>
2023-07-29 12:46:06 +0300

    Fix reselect bottom nav item from other destinations issue

--- app/src/main/java/com/example/fakestoreandro/MainActivity.kt
+++ app/src/main/java/com/example/fakestoreandro/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.fakestoreandro
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.fakestoreandro.databinding.ActivityMainBinding
@@ -12,6 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,18 +23,59 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        initView()
+    }
+
+    private fun initView() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         binding.bottomNavigation.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             binding.bottomNavigation.isVisible = destination.id != R.id.productDetailFragment
+
+            onBottomNavItemReselected(destination)
         }
 
         val badge = binding.bottomNavigation.getOrCreateBadge(R.id.basket_nav_graph)
         badge.isVisible = true
         badge.number = 4
+    }
+
+    private fun onBottomNavItemReselected(
+        destination: NavDestination,
+    ) {
+        binding.bottomNavigation.setOnItemReselectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home_nav_graph -> {
+                    val startDestinationOfHomeGraph = R.id.homeFragment
+
+                    popIfNotStartDestination(destination.id, startDestinationOfHomeGraph)
+                }
+
+                R.id.basket_nav_graph -> {
+                    val startDestinationOfHomeGraph = R.id.basketFragment
+
+                    popIfNotStartDestination(destination.id, startDestinationOfHomeGraph)
+                }
+
+                R.id.profile_nav_graph -> {
+                    val startDestinationOfHomeGraph = R.id.profileFragment
+
+                    popIfNotStartDestination(destination.id, startDestinationOfHomeGraph)
+                }
+            }
+        }
+    }
+
+    private fun popIfNotStartDestination(
+        destinationId: Int,
+        startDestinationOfHomeGraph: Int
+    ) {
+        if (destinationId != startDestinationOfHomeGraph) {
+            navController.popBackStack(destinationId, true)
+        }
     }
 }
